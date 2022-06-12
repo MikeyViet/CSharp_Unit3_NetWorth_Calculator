6d20c1e111f3f889da94ca74d6768817cc3412af
cuviet <vietwiin@gmail.com>
2022-06-12 17:30:14 -0500

    Populated Lists and got output

--- CSharp_Unit3_NetWorth_Calculator/NetWorth_Nguyen.cs
+++ CSharp_Unit3_NetWorth_Calculator/NetWorth_Nguyen.cs
@@ -21,8 +21,56 @@ namespace Networth
 {
     class NetWorth_Nguyen
     {
+
         static void Main(string[] args)
         {
+            //variable definitions
+            int maxAssets = 0;
+            int maxLiabilities = 0;
+            double assetsInput = 0;
+            double liabilitiesInput = 0;
+            double totalAssets = 0;
+            double totalLiabilities = 0;
+            
+            //create lists for assets and liabilities
+            List<double> assets = new List<double>();
+            List<double> liabilities = new List<double>();
+
+            //prompt for user input
+            Console.Write("Please enter the number of assets you have: ");
+            maxAssets = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < maxAssets; i++)
+            {
+                Console.Write($"Please enter in asset #{i + 1}: ");
+                assetsInput = double.Parse(Console.ReadLine());
+                assets.Add(assetsInput);    //add asset to list
+                totalAssets += assetsInput;
+            }
+
+            Console.Write("Please enter the number of liabilites you have: ");
+            maxLiabilities = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < maxLiabilities; i++)
+            {
+                Console.Write($"Please enter in liability #{i + 1}: ");
+                liabilitiesInput = double.Parse(Console.ReadLine());
+                liabilities.Add(liabilitiesInput); //add liability to list
+                totalLiabilities += liabilitiesInput;
+            }
+
+
+            Console.WriteLine("\n");
+
+            for(int i = 0; i < maxAssets; i++)
+            {
+                Console.WriteLine(assets[i]);
+            }
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine($"Your Networth is: {totalAssets - totalLiabilities}.");
+
             Console.WriteLine("Hello, World!");
         }
     }
