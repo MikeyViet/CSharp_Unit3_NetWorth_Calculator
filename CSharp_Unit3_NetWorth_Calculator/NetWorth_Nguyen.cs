6e733b6d19cd4ca1c24c6a056380426248be70e5
cuviet <cuviet@MICHAELNGUY47B5>
2022-06-12 15:28:02 -0500

    Add project files.

---
+++ CSharp_Unit3_NetWorth_Calculator/NetWorth_Nguyen.cs
@@ -0,0 +1,28 @@
+﻿/***************************************************************************
+ * Assignment#: 3
+ * Name:        The Faces of C# 1
+ * Date:        06/12/2022
+ * Class:       COSC-1430 C#
+ * Student:     Michael Nguyen
+ * Professor:   Nicholas Osemeke
+ * 
+ * Description: The program will calculate a person's networth from user
+ *              input. The program will prompt user to input Assets and 
+ *              liabilites and store each of them in a separate list. The
+ *              networth will be calculated by subtracting total liabilities
+ *              from total assets. Network will be displayed on screen.
+ ****************************************************************************/
+
+using System;
+
+namespace Networth
+{
+    class NetWorth_Nguyen
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
